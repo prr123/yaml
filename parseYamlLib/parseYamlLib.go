49dc52e608c8c13773ae69b0b296fbdaaa51d295
prr123 <priemen@gmail.com>
2022-07-13 17:14:46 +0200

    initial commit

---
+++ parseYamlLib/parseYamlLib.go
@@ -0,0 +1,14 @@
+// parseYamlLib
+// program that parses a yaml file and generates go lang code for reading
+// author: prr, azul software
+// date: 13 July 2022
+// copyright 2022 prr, azul software
+//
+
+package parseYamlLib
+
+import (
+    "os"
+    "fmt"
+)
+
